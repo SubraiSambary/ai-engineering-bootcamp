a72921beb02bb2e3bf88984d345df77ffb2186cc
Subrai Sambary <subraisambary@outlook.com>
2026-02-22 00:58:54 +0530

    Day 1: Initial setup and AI CLI tool

---
+++ week1_intro_cli.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+from groq import Groq
+
+load_dotenv()
+
+client = OpenAI(
+    base_url="https://api.groq.com/openai/v1",
+    api_key=os.getenv("OPENAI_API_KEY")
+    )
+
+def generate_response(topic):
+    prompt = f"""
+    Explain {topic} in a simple but technically accurate way.
+    Keep it structured.
+    """
+    response = client.chat.completions.create(
+        model="openai/gpt-oss-120b",
+        messages=[
+            {"role": "user", 
+             "content": prompt}
+        ]
+    )
+
+    return response.choices[0].message.content
+
+if __name__ == "__main__":
+    topic = input("Enter a topic: ")
+    response = generate_response(topic)
+    print("\n--- AI Response ---\n")
+    print(response)
